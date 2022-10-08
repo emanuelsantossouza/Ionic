e75f784cd8bed89f77b34bf37f062c7bf7bcb007
Emanuel Santos Souza de Jesus <emanuelsantoss42@gmail.com>
2022-10-07 21:50:53 -0300

    Add Camera

--- App-Calculadora/src/app/tab2/tab2.page.ts
+++ App-Calculadora/src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,24 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  operacao = '';
+  resultado = '';
+  constructor() { }
 
+  mais(valor: string) {
+    this.operacao += valor;
+  }
+  limpar() {
+    this.operacao = '';
+  }
+  limparMemoria() {
+    this.operacao = '';
+    this.resultado = '';
+  }
+
+  apagarCaracter() {
+    if (this.operacao.length > 0) {
+      this.operacao = this.operacao.substring(0, this.operacao.length - 1);
+    }
+  }
 }
